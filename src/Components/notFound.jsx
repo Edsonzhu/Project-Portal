c78a353f8528e6ebdb37c6c6377bcf461ea36094
Edson <efkzhu@myseneca.ca>
2019-08-26 09:58:46 -0400

    Not found component done

---
+++ src/Components/notFound.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import MainContainer from './mainContainer';
+
+class NotFound extends Component {
+
+    render() {
+        return(
+            <MainContainer>
+                <h1 className="page-header">Not Found</h1>
+                <span>Page Not Found</span>
+            </MainContainer>
+        )
+    }
+}
+
+export default NotFound;
