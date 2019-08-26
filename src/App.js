905d4256f47c5a2335fbb9735c03b27d8df6e5b3
Edson <efkzhu@myseneca.ca>
2019-08-26 00:21:57 -0400

    Project view done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Overview from './Components/overview';
+import Projects from './Components/projects';
 
 class App extends Component {
   render() {
     return (
-      <Overview />
+      // <Overview />
+      <Projects />
     );
   }
 }
--- src/Components/projects.jsx
+++ src/Components/projects.jsx
@@ -4,47 +4,56 @@ import axios from 'axios';
 import moment from 'moment';
 
 class Projects extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            projects: []
-        };
-        this.fetchProjects = this.fetchProjects.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: []
+    };
+    this.fetchProjects = this.fetchProjects.bind(this);
+  }
 
-    componentDidMount() {
-        this.fetchProjects();
-    }
+  componentDidMount() {
+    this.fetchProjects();
+  }
 
-    fetchProjects() {
-        axios.get(`https://shrouded-crag-99446.herokuapp.com/projects`)
-        .then((res) => {this.setState({ projects: res.data })})
-        .catch((err) => { console.log("Error - Fetch Project (Project.jsx) -- " + err ) })
-    }
+  fetchProjects() {
+    axios
+      .get(`https://shrouded-crag-99446.herokuapp.com/projects`)
+      .then(res => {
+        this.setState({ projects: res.data });
+      })
+      .catch(err => {
+        console.log("Error - Fetch Project (Project.jsx) -- " + err);
+      });
+  }
 
-    render() {
-        return (
-            <MainContainer sideBar="Projects">
-                <h1 className="page-header">Projects</h1>
-                <table className="table table-striped table-bordered">
-                    <thead>
-                        <td>Name</td>
-                        <td>Description</td>
-                        <td>Start Date</td>
-                        <td>End Date</td>
-                    </thead>
-                    {this.state.projects.map(project => 
-                        <tr>
-                            <td>{ project.ProjectName }</td>
-                            <td>{ project.Description }</td>
-                            <td>{ project.StartDate }</td>
-                            <td>{ project.EndDate }</td>
-                        </tr>
-                        )}
-                </table>
-            </MainContainer>
-        )
-    }
+  render() {
+    return (
+      <MainContainer sideBar="Projects">
+        <h1 className="page-header">Projects</h1>
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Description</th>
+              <th scope="col">Start Date</th>
+              <th scope="col">End Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.projects.map(project => (
+              <tr key={project._id}>
+                <td>{project.ProjectName}</td>
+                <td>{project.ProjectDescription}</td>
+                <td>{moment(project.ProjectStartDate).utc().format('LL')}</td>
+                <td>{project.ProjectEndDate === null ? ('n/a') : (moment(project.ProjectEndDate).format('LL'))}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </MainContainer>
+    );
+  }
 }
 
 export default Projects;
